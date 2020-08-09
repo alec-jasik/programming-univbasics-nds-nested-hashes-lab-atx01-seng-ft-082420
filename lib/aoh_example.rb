f958bc92643acc916788bb3336ca2b01317bde4c
Alec Jasik <alec_jasik@yahoo.com>
2020-08-09 18:44:35 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -9,7 +9,7 @@ BRUCE_G = { name:  "Bruce Green", occupation: "Fan of Mildred" }
 def assembled_aoh
   [{name: "Don Gately", occupation: "Live-in Staff"},
   {name:  "Joelle van Dyne", occupation: "Radio Personality"}
-  
+  cupation: "Staff" }
   ]
 end
 
