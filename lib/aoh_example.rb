6098d30ded2a35fb3462310deb6ecbee72e546d1
Alec Jasik <alec_jasik@yahoo.com>
2020-08-09 18:50:20 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -10,12 +10,13 @@ def assembled_aoh
 end
 
 def literal_aoh
-  [{name: "Don Gately", occupation: "Live-in Staff"},
+ litral = [{name: "Don Gately", occupation: "Live-in Staff"},
   {name:  "Joelle van Dyne", occupation: "Radio Personality"},
   {name:  "Pat Monteseian", occupation: "Staff"},
   {name:  "Kate Gompert", occupation: "None"},
   {name:  "Bruce Green", occupation: "Fan of Mildred"}
-  ].sort 
+  ]
+  litral.sort
 end
 
 def aoh_lookup(aoh, row, key)
